2bf247575c3ebb14065cde667ecf8bcc6403dbed
AntonMUPPP <ante@po9.se>
2023-12-09 21:51:43 +0100

    added 10 true or false questions and correct check

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
 let questions = [
     {
         id: "q1",
-        question: "ratatatatatat",
+        question: "The Great Wall of China is visible from the moon.",
         answers: `<label><input type="radio" name="answer1" id="true"> True</label>
-        <label><input type="radio" name="answer1" id="false"> False</label>`
+        <label><input type="radio" name="answer1" id="false"> False</label>`,
+        correctAnswerId: "false"
     },
     {
         id: "q2",
-        question: "How many moons does Jupiter have",
+        question: "The Earth is flat.",
         answers: `<label><input type="radio" name="answer2" id="true"> True</label>
-        <label><input type="radio" name="answer2" id="false"> False</label>`
+        <label><input type="radio" name="answer2" id="false"> False</label>`,
+        correctAnswerId: "false"
     },
     {
         id: "q3",
-        question: "hur många bollar finns",
-        answers:`<label><input type="radio" name="answer3" id="5"> 5</label>
-        <label><input type="radio" name="answer3" id="50"> 50</label>
-        <label><input type="radio" name="answer3" id="500"> 500</label>
-        <label><input type="radio" name="answer3" id="5000"> 5000</label>`
+        question: "The sun is a planet.",
+        answers:`<label><input type="radio" name="answer3" id="true"> True</label>
+        <label><input type="radio" name="answer3" id="false"> False</label>`,
+        correctAnswerId: "false"
+    },
+    {
+        id: "q4",
+        question: "Bananas are berries.",
+        answers:`<label><input type="radio" name="answer4" id="true"> True</label>
+        <label><input type="radio" name="answer4" id="false"> False</label>`,
+        correctAnswerId: "true"
+    },
+    {
+        id: "q5",
+        question: "Mount Everest is the tallest mountain in the world.",
+        answers:`<label><input type="radio" name="answer5" id="true"> True</label>
+        <label><input type="radio" name="answer5" id="false"> False</label>`,
+        correctAnswerId: "true"
+    },
+    {
+        id: "q6",
+        question: "The Eiffel Tower is made entirely of iron.",
+        answers:`<label><input type="radio" name="answer6" id="true"> True</label>
+        <label><input type="radio" name="answer6" id="false"> False</label>`,
+        correctAnswerId: "false"
+    },
+    {
+        id: "q7",
+        question: "The Great Barrier Reef is the largest living structure on Earth.",
+        answers:`<label><input type="radio" name="answer7" id="true"> True</label>
+        <label><input type="radio" name="answer7" id="false"> False</label>`,
+        correctAnswerId: "true"
+    },
+    {
+        id: "q8",
+        question: "The moon has its own light source.",
+        answers:`<label><input type="radio" name="answer8" id="true"> True</label>
+        <label><input type="radio" name="answer8" id="false"> False</label>`,
+        correctAnswerId: "false"
+    },
+    {
+        id: "q9",
+        question: "The Sahara Desert is the largest desert in the world.",
+        answers:`<label><input type="radio" name="answer9" id="true"> True</label>
+        <label><input type="radio" name="answer9" id="false"> False</label>`,
+        correctAnswerId: "true"
+    },
+    {
+        id: "q10",
+        question: "The currency of Japan is the yuan.",
+        answers:`<label><input type="radio" name="answer10" id="true"> True</label>
+        <label><input type="radio" name="answer10" id="false"> False</label>`,
+        correctAnswerId: "false"
     },
 ]
 
 let wrapper = document.querySelector(".wrapper")
+let submitBtn = document.createElement("button")
+let allText = document.getElementsByTagName("h2")
 
 function startQuiz(){
     const tempElement = document.querySelector(".temp-element")
@@ -43,8 +95,34 @@ function startQuiz(){
         questionContainer.append(temp)
     }
 
-    let submitBtn = document.createElement("button")
     const submitContainer = document.querySelector(".submit-btn")
     submitBtn.innerText = "Submit"
     submitContainer.append(submitBtn)
 }
+
+submitBtn.addEventListener("click", () => {
+    let selectedAnswers = []
+    let correctAnswersCount = 0
+
+    for (let i = 0; i < questions.length; i++) {
+        let questionContainer = document.querySelectorAll(".question-container")[i]
+        let selectedRadio = questionContainer.querySelector('input[type="radio"]:checked')
+
+        if (selectedRadio) {
+            let userAnswerId = selectedRadio.id
+            let isCorrect = userAnswerId === questions[i].correctAnswerId
+
+            selectedAnswers.push({
+                userAnswerId: userAnswerId,
+                isCorrect: isCorrect
+            })
+
+            if (isCorrect) {
+                correctAnswersCount++
+            }
+        }
+    }
+
+    console.log(selectedAnswers)
+    console.log("Correct Answers Count:", correctAnswersCount)
+})
