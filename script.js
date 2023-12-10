03b3050462f877d3a3a16c756f6c6d4df4d206e6
AntonMUPPP <ante@po9.se>
2023-12-10 15:17:25 +0100

    added comments

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ let questions = [
         correctAnswerId: "false"
     },
 ]
+//Array of all the questions
 
 let wrapper = document.querySelector(".wrapper")
 let submitBtn = document.createElement("button")
@@ -83,11 +84,10 @@ let allText = document.getElementsByTagName("h2")
 const header = document.querySelector(".header")
 const myh1 = document.querySelector("h1")
 const themeBtn = document.querySelector(".theme-btn")
-const resultWindow = document.createElement("div")
-resultWindow.classList.add("result-window")
 
-for (let i = 0; i < questions.length; i++){
-    let questionContainer = document.createElement("div")
+
+for (let i = 0; i < questions.length; i++){     //For loop to append all of the questions from the array onto the page
+    let questionContainer = document.createElement("div")       //Also adding pre-made classes to give them some styling
     questionContainer.classList.add("question-container")
 
     let temp = document.createElement("div")
@@ -102,68 +102,72 @@ for (let i = 0; i < questions.length; i++){
     questionContainer.append(temp)
 }
 
-const submitContainer = document.querySelector(".submit-btn")
-    submitBtn.innerText = "Submit"
-    submitContainer.append(submitBtn)
+const submitContainer = document.querySelector(".submit-btn")   //Finds the div in the HTML that has the submit-btn class so the actual button goes in the right place
+submitBtn.innerText = "Submit"
+submitContainer.append(submitBtn)
 
+//Function for the submit button
 submitBtn.addEventListener("click", () => {
-    window.scrollTo(0,0)
+    window.scrollTo(0,0)    //Scrolls automatically to the top of the page after the button is pressed so you can see what questions you got wrong/right
     
     let selectedAnswers = []
     let correctAnswersCount = 0
 
-    for (let i = 0; i < questions.length; i++) {
+    for (let i = 0; i < questions.length; i++) {        //For loop to get all the users answers
         let questionContainer = document.querySelectorAll(".question-container")[i]
         let selectedRadio = questionContainer.querySelector('input[type="radio"]:checked')
 
-        if(selectedRadio){
+        if(selectedRadio){      //Checks if the user answered the question or not
             let userAnswerId = selectedRadio.id
             let isCorrect = userAnswerId === questions[i].correctAnswerId
 
-            selectedAnswers.push({
+            selectedAnswers.push({      //Pushes the users inputs to an array as an object per question
                 userAnswerId: userAnswerId,
                 isCorrect: isCorrect
             })
 
-            if(isCorrect){
-                correctAnswersCount++
+            if(isCorrect){      //Then checks if they got it right or not
+                correctAnswersCount++       //If they did it increases their score and changes the background color of the question they got right to green
                 questionContainer.style.backgroundColor = "green"
             }else if(!isCorrect){
-                questionContainer.style.backgroundColor = "red"
+                questionContainer.style.backgroundColor = "red"     //If they got it wrong, turns the background color red instead
             }
         }
-        else{
-            console.log(`question ${i+1} is unanswered`)
+        else{       //If they didnt, changes the color to gray to show that they missed or skipped a question
             questionContainer.style.backgroundColor = "gray"
         }
     }
 
-    console.log(selectedAnswers)
-    console.log("Correct Answers Count:", correctAnswersCount)
+    //console.log(selectedAnswers)      debugging stuff
+    //console.log("Correct Answers Count:", correctAnswersCount)        debugging stuff
 
-    const allAnswers = document.getElementsByClassName("answer-container")
+    const allAnswers = document.getElementsByClassName("answer-container")      //Gets all the answer containers
+    //Using Array.from() to convert allAnswers into an array from an HTML collection
     Array.from(allAnswers).forEach((answer) => {
-        answer.remove()
+        answer.remove()     //Then removes the answer from each question just so you cant change the answer after you have submitted
     })
     submitBtn.remove()
 
     const score = document.createElement("h3")
     const resultText = document.createElement("p")
-
+    const resultWindow = document.createElement("div")
+    resultWindow.classList.add("result-window")
     wrapper.append(resultWindow)
+
     score.innerText = `${correctAnswersCount}/10`
+    //If statement to check the users score in % and then changes the color and text accordingly
     if(correctAnswersCount >= questions.length * 0.75){
         resultText.style.color = "green"
         score.style.color = "green"
-        resultText.innerText = "Riktigt bra jobbat!"
+        resultText.innerText = "Incredibly well done!"
     }else if(correctAnswersCount >= questions.length * 0.50){
         resultText.style.color = "orange"
         score.style.color = "orange"
-        resultText.innerText = "Bra jobbat!"
+        resultText.innerText = "Well done!"
     }else if(correctAnswersCount <= questions.length * 0.50){
         resultText.style.color = "red"
         score.style.color = "red"
-        resultText.innerText = "Underkänd!"
+        resultText.innerText = "You did not pass!"
     }
     resultWindow.append(score)
     resultWindow.append(resultText)
@@ -171,19 +175,24 @@ submitBtn.addEventListener("click", () => {
     const retakeBtn = document.createElement("button")
     retakeBtn.innerText = "Retake Quiz"
     submitContainer.append(retakeBtn)
+    //Adds a retake quiz button but its not very advanced, just reloads the page and scrolls all the way up
     retakeBtn.addEventListener("click", () => {
         location.reload()
         window.scrollTo(0,0)
     })
 })
 
+
 function changeTheme(){
+    //Toggles darkmode classes on the elements that should be affected
+    //Also toggles a class on the theme button to create a cool transition and hover effect
     document.body.classList.toggle("darkmode-bg")
     header.classList.toggle("darkmode-header")
     myh1.classList.toggle("darkmode-txt")
     themeBtn.classList.toggle("active")
 
     let questionContainers = document.querySelectorAll(".question-container")
+    //Goes through each container in questionContainers and gets the h2 to change theme
     questionContainers.forEach(container => {
         let questionName = container.querySelector("h2")
         questionName.classList.toggle("darkmode-txt")
